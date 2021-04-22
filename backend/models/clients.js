c5c8446d015869b6c8726524f9d3e49ba0e29bd7
Musah Abdul-Malik <musah.abdulmalikgh@gmail.com>
2021-04-22 09:02:51 +0000

    Add client schema

--- backend/models/clients.js
+++ backend/models/clients.js
@@ -1,1 +1,24 @@
-const 
+const mongoose = require('mongoose')
+const {isEmail} = require('validator')
+
+const clientSchema = ({
+    name: {
+        type:String,
+        required:[true, 'Name is required']
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        validate: [isEmail, 'Enter a valid email']
+    },
+    phone: {
+        type:Number,
+        required:[true, "Phone number is required"]
+    },
+    providers: {
+        type:Array,
+        required: [true, "Providers is required"]
+    }
+})
+
+module.exports = mongoose.model('Client', clientSchema)
